dd5635e46ff21e7d95d245532fbd963c01b8623a
manojtiwari27 <tiwarimanoj2709@gmail.com>
2023-02-02 18:56:06 +0530

    Second Commit

--- dayone/src/org/tnsif/intro/DataTypesDemo.java
+++ dayone/src/org/tnsif/intro/DataTypesDemo.java
@@ -1,42 +1,42 @@
 package org.tnsif.intro;
-
 public class DataTypesDemo {
 //Performing data type operations
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		byte a = 127;
-		//byte = 1 byte
+		//byte = 1 byte byte contains values between -128 to 127
 		int  b = 128;
 		//int = 4 byte
 		short b1 = 135;
 		long b2 = 654646464l;
-		float c = 12/5;
-		float d = 5/2.5f;
-		double e = 12/5;
-		double f = 12d/5d;
+		float f1 = 12/5;
+		float f2 = 5/2.5f;
+		double d1 = 12/5;
+		double d2 = 12d/5d;
 		
+		// character
+		char ch = 'M';
 		
-		//character
-		char g = 'M';
-		//Ascci Value
-		int h = 'A';
+		// Ascci Value
+		int ch1 = 'A';
 		
+		// String
 		String str = "Manoj";
-		//Boolean 
-		boolean i = true;
 		
-		System.out.println("The Value byte of a is: " +a);
-		System.out.println("The Value int  of b is: " +b);
-		System.out.println("The Value short of b1 is: " +b1);
-		System.out.println("The Value long of b2 is: " +b2);
-		System.out.println("The Value float of c is: " +c);
-		System.out.println("The Value float of d is: " +d);
-		System.out.println("The Value double of e is: " +e);
-		System.out.println("The Value double of f is: " +f);
-		System.out.println("The Value char of g is: " +g);
-		System.out.println("ascci value of A is: " +h);
+		//Boolean 
+		boolean bool = true;
+		
+		System.out.println("The byte value of a is: " +a);
+		System.out.println("The int value  of b is: " +b);
+		System.out.println("The short value of b1 is: " +b1);
+		System.out.println("The long value of b2 is: " +b2);
+		System.out.println("The float value of f1 is: " +f1);
+		System.out.println("The float value of f2 is: " +f2);
+		System.out.println("The double Value of d1 is: " +d1);
+		System.out.println("The double Value of d2 is: " +d2);
+		System.out.println("The char value of ch is: " +ch);
+		System.out.println("ascci value of A is: " +ch1);
 		System.out.println("The Value of str is: " +str);
-		System.out.println("The Value of boolean i is: " +i);
+		System.out.println("The Value of boolean bool is: " +bool);
 		
 		
 		
