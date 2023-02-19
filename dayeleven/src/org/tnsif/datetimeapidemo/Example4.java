1554cd93b28a4d7c16c0e093b9a6a3ba226ad364
manojtiwari27 <tiwarimanoj2709@gmail.com>
2023-02-19 17:26:26 +0530

    First Commit

---
+++ dayeleven/src/org/tnsif/datetimeapidemo/Example4.java
@@ -0,0 +1,14 @@
+package org.tnsif.datetimeapidemo;
+import java.time.Clock;
+
+public class Example4 
+{
+	public static void main(String[] args) 
+	{
+		@SuppressWarnings("unused")
+		Clock c=Clock.systemUTC();
+		Clock c1=Clock.systemDefaultZone();
+		System.out.println(c1);
+	}
+
+}
